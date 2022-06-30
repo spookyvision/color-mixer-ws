df9a836ddc61df9dcfcbe72fcdfa1c64883db358
Anatol Ulrich <45840+spookyvision@users.noreply.github.com>
2022-06-30 05:49:56 +0200

    oof

--- mixer-dioxus/src/main.rs
+++ mixer-dioxus/src/main.rs
@@ -1,23 +1,32 @@
 use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::{RefCell, RefMut},
+    ops::{Deref, DerefMut},
     rc::Rc,
-    sync::{mpsc, Mutex},
+    sync::{atomic::AtomicBool, mpsc, Condvar, Mutex},
 };
 
+use chrono::{Duration, Utc};
 use color_mixer::strip::{Control, Segment, Srgb8, State, Wrap};
 use dioxus::{core::to_owned, prelude::*};
 use fermi::{use_atom_state, use_init_atom_root, use_read, use_set, Atom};
+use futures::StreamExt;
+// use futures_channel::mpsc::{unbounded, UnboundedReceiver};
 use gloo::timers::future::TimeoutFuture;
 use indexmap::IndexMap;
+use log::debug;
 use palette::{stimulus::IntoStimulus, Srgb};
 
-pub static STATE_ATOM: Atom<Option<State>> = |_| None;
+pub static STATE_ATOM: Atom<Option<SegMap>> = |_| None;
 
 const BASE_URL: Option<&'static str> = Some("http://127.0.0.1:8081/");
+
+type Res<T> = Result<T, Box<dyn std::error::Error>>;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     console_error_panic_hook::set_once();
-    dioxus::web::launch(AppOuter);
+    dioxus::web::launch(AppOutest);
 }
 
 #[allow(non_snake_case)]
@@ -42,28 +51,107 @@ fn Color2(
     }))
 }
 
+type SegMap = IndexMap<String, Segment>;
+
 #[derive(Debug, Clone)]
-struct DState(Rc<Mutex<IndexMap<String, Segment>>>);
+struct DState(Rc<RefCell<SegMap>>);
+
+// impl DState {
+//     fn inner_mut<'cx>(&self, cx: &'cx ScopeState) -> DStateMut<'_, 'cx> {
+//         let inner = RefCell::borrow_mut(&self.0);
+//         DStateMut { inner, cx }
+//     }
+// }
+// struct DStateMut<'i, 'cx: 'i> {
+//     inner: RefMut<'i, SegMap>,
+//     cx: &'cx ScopeState,
+// }
+
+// impl<'i, 'cx> Deref for DStateMut<'i, 'cx> {
+//     type Target = SegMap;
+
+//     #[inline]
+//     fn deref(&self) -> &Self::Target {
+//         &self.inner
+//     }
+// }
+
+// impl<'i, 'cx> DerefMut for DStateMut<'i, 'cx> {
+//     #[inline]
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         &mut self.inner
+//     }
+// }
+
+// impl<'i, 'cx> Drop for DStateMut<'i, 'cx> {
+//     fn drop(&mut self) {
+//         if let Some(update) = self.cx.consume_context::<UpdateState>() {
+//             log::debug!("sending drop-da-update!");
+//             update.0.send(());
+//         }
+//     }
+// }
 
 #[allow(non_snake_case)]
 #[inline_props]
 fn ColorInput(cx: Scope, segment_id: String, color_idx: usize, val: UseState<Srgb8>) -> Element {
-    let segments = cx.use_hook(|_| cx.consume_context::<DState>());
+    let segments = use_atom_state(&cx, STATE_ATOM);
+
+    to_owned![val];
+    let val_too = val.clone();
+    let val_three = val.clone();
+
+    // if let Some(base_url) = BASE_URL {
+    //     let update: &UseFuture<Res<()>> = use_future(&cx, &val, |val| async move {
+    //         let now = Utc::now();
+    //         let debounce_amount = std::time::Duration::from_millis(400);
+    //         let dt = now
+    //             .signed_duration_since(*last_update.read())
+    //             .to_std()
+    //             .unwrap_or(debounce_amount);
+
+    //         if let Some(wait) = debounce_amount.checked_sub(dt) {
+    //             log::debug!("debounce: {wait:?}");
+    //             TimeoutFuture::new(wait.as_millis() as u32).await;
+    //         }
+
+    //         log::debug!("updating");
+    //         let url = format!("{base_url}data");
+    //         let dat = serde_json::to_vec(&*segments_too.unwrap().0.borrow())?;
+    //         let mut req = surf::post(url).body_bytes(&dat).await?;
+    //         let _loaded = req.body_bytes();
+
+    //         last_update.with_mut(|set| *set = now);
+
+    //         Ok(())
+    //     });
+    // }
+
+    let update = cx.consume_context::<UpdateState>();
 
     cx.render(rsx! {
         input {
             r#type: "color",
-            value: format_args!("#{:x}", **val),
+            value: format_args!("#{:x}", * val_too),
             oninput: move |ev| {
                 let color: Srgb8 = ev.value.parse().unwrap();
-                val.set(color);
-                log::debug!("{segments:?}");
-                if let Some(segments) = segments {
-                    let mut segments = segments.0.lock().unwrap();
-                    if let Some(segment) = segments.get_mut(segment_id) {
-                        segment.colors_mut()[*color_idx] = Wrap(color);
-                    }
-                }
+                val_three.set(color);
+                    segments.modify(|segments|
+                        {
+                            let mut segments = segments.to_owned();
+                            if let Some(ref mut segments) = segments {
+                                if let Some(segment) = segments.get_mut(segment_id) {
+                                    segment.colors_mut()[*color_idx] = Wrap(color);
+
+                                    if let Some(ref update) = update {
+                                        update.0.send(segments.clone());
+                                    }
+                                }
+                            }
+
+                            segments
+                        }
+                    );
             },
         }
     })
@@ -82,72 +170,115 @@ fn SegmentN(cx: Scope, seg: Segment, prime_idx: usize, fac: u32, now: u32) -> El
     ))
 }
 
+#[derive(Clone)]
+struct UpdateState(CoroutineHandle<SegMap>);
+
 #[allow(non_snake_case)]
 #[inline_props]
-fn Segments(cx: Scope, state: State, fac: UseState<String>, now: u32) -> Element {
+fn Segments(cx: Scope, fac: UseState<String>, now: u32) -> Element {
     let fac: u32 = fac.get().parse().unwrap();
-    cx.use_hook(|_| {
-        cx.provide_context(DState(Rc::new(Mutex::new(state.segments().clone()))));
-    });
 
-    let content = state.iter().map(|(segment_id, seg)| {
-        rsx! {
-            div {
-                key: "seg-{segment_id}",
-                SegmentN{seg:seg.clone(), prime_idx: seg.chill_idx(), fac: fac, now: *now}}
+    let global_segments = use_read(&cx, STATE_ATOM);
+
+    let update = use_coroutine(&cx, |mut rx: UnboundedReceiver<SegMap>| async move {
+        if let Some(base_url) = BASE_URL {
+            let mut last_update = Utc::now();
+
+            let inner = async move {
+                while let Some(mut data) = rx.next().await {
+                    loop {
+                        match rx.try_next() {
+                            Ok(None) => {
+                                log::info!("shutting down updater");
+                            }
+                            Ok(Some(next_data)) => {
+                                log::debug!("but wait, there's more!");
+                                data = next_data;
+                                continue;
+                            }
+                            Err(data) => {
+                                log::debug!("I can't believe it's not bu^Wmore!");
+                                break;
+                            } // channel has been drained
+                        }
+                    }
+
+                    let now = Utc::now();
+                    let debounce_amount = std::time::Duration::from_millis(2_000);
+
+                    let dt = now
+                        .signed_duration_since(last_update)
+                        .to_std()
+                        .unwrap_or(debounce_amount);
+
+                    if let Some(wait) = debounce_amount.checked_sub(dt) {
+                        log::debug!("debounce: {wait:?}");
+                        TimeoutFuture::new(wait.as_millis() as u32).await;
+                    }
+                    log::debug!("updating!");
+
+                    let url = format!("{base_url}data");
+
+                    let ser = serde_json::to_vec(&data)?;
+                    let mut req = surf::post(url).body_bytes(&ser).await?;
+                    let _loaded = req.body_bytes().await;
+
+                    last_update = Utc::now();
+                }
+                Ok(())
+            };
+
+            let res: Res<()> = inner.await;
         }
     });
+
+    cx.provide_context(UpdateState(update.to_owned()));
+
+    let content = match global_segments {
+        None => rsx!(div {"loading..."}),
+        Some(segments) => {
+            let inner = segments.iter().map(|(segment_id, seg)| {
+                rsx! {
+                    div {
+                        key: "seg-{segment_id}",
+                        SegmentN{seg:seg.clone(), prime_idx: seg.chill_idx(), fac: fac, now: *now}}
+                }
+            });
+            rsx!(div { inner })
+        }
+    };
 
     cx.render(rsx!(div { content }))
 }
 
+fn AppOutest(cx: Scope) -> Element {
+    use_context_provider(&cx, || SegMap::new());
+    cx.render(rsx!(AppOuter {}))
+}
 fn AppOuter(cx: Scope) -> Element {
     let set_state = Rc::clone(use_set(&cx, STATE_ATOM));
 
-    let state = State::new(
-        [
-            Segment::new(
-                1,
-                false,
-                Srgb8::new(255, 150, 0),
-                Srgb8::new(255, 10, 120),
-                0,
-                100,
-            ),
-            Segment::new(
-                1,
-                false,
-                Srgb8::new(166, 0, 255),
-                Srgb8::new(2, 192, 192),
-                1,
-                100,
-            ),
-            Segment::new(
-                1,
-                false,
-                Srgb8::new(20, 200, 141),
-                Srgb8::new(200, 176, 20),
-                2,
-                100,
-            ),
-            Segment::new(
-                1,
-                false,
-                Srgb8::new(200, 20, 30),
-                Srgb8::new(200, 200, 10),
-                3,
-                100,
-            ),
-        ]
-        .into_iter(),
-    );
+    if let Some(base_url) = BASE_URL {
+        cx.spawn({
+            async move {
+                let inner = async move {
+                    let url = format!("{base_url}data");
+                    let mut res = surf::get(url).await?;
+                    let body = res.body_bytes().await?;
+                    let loaded_segments: IndexMap<String, Segment> = serde_json::from_slice(&body)?;
+                    debug!("loaded {loaded_segments:?}");
+                    set_state(Some(loaded_segments));
+                    Ok(())
+                };
+                let res: Res<()> = inner.await;
+            }
+        });
+    }
 
-    set_state(Some(state));
     cx.render(rsx!(App {}))
 }
 
 fn App(cx: Scope) -> Element {
-    let state: &Option<State> = use_read(&cx, STATE_ATOM);
     let control = use_ref(&cx, || Control::new());
 
     let now = control.write().tick();
@@ -164,18 +295,17 @@ fn App(cx: Scope) -> Element {
     let delta_too = delta.clone();
     let now_too = now.clone();
     let mut old_delta = 0;
-    let _english_setter: &UseFuture<Result<_, Box<dyn std::error::Error>>> =
-        use_future(&cx, &control, |c| async move {
-            let dat_now = c.with_mut(|c| c.tick());
-            let mut new_delta = *delta;
-            now_too.set(dat_now + (*delta as u32));
-            Ok(())
-        });
+    let _english_setter: &UseFuture<Res<_>> = use_future(&cx, &control, |c| async move {
+        let dat_now = c.with_mut(|c| c.tick());
+        let mut new_delta = *delta;
+        now_too.set(dat_now + (*delta as u32));
+        Ok(())
+    });
 
     let local_now = now.clone();
-    let _irish_setter: &UseFuture<Result<_, Box<dyn std::error::Error>>>;
-    if let Some(base_url) = BASE_URL {
-        _irish_setter = use_future(&cx, (), |_| async move {
+
+    let _irish_setter: &UseFuture<Res<_>> = use_future(&cx, (), |_| async move {
+        if let Some(base_url) = BASE_URL {
             loop {
                 let url = format!("{base_url}now");
                 let mut res = surf::get(url).await?;
@@ -186,38 +316,35 @@ fn App(cx: Scope) -> Element {
 
                 delta_too.with_mut(|set| *set = delta_value);
 
-                TimeoutFuture::new(1_000).await;
+                TimeoutFuture::new(2_000).await;
             }
-            Ok(())
-        });
-    }
+        }
+        Ok(())
+    });
 
-    let content = match state {
-        None => rsx!(p { "loading..." }),
-        Some(state) => rsx! (
-             div {
-                 style: "text-align: center;",
-                 h1 { "Bisexual lighting controller" }
-                 h3 { "(bisexuality optional)" }
-                 p { "time: {now}"}
-                 form {
-                     input {
-                         r#type: "range",
-                         name: "chill_val",
-                         value: "{chill_val}",
-                         id: "chill_val",
-                         min: "10",
-                         max: "800",
-                         oninput: move |ev| chill_val.set(ev.value.clone()),
-                     }
-                 }
-                 p { "chill: {chill_val}"}
-
-                 Segments {state: state.clone(), fac: chill_val.clone(), now: **now}
-
-
+    let content = rsx! (
+     div {
+         style: "text-align: center;",
+         h1 { "Bisexual lighting controller" }
+         h3 { "(bisexuality optional)" }
+         p { "time: {now}"}
+         form {
+             input {
+                 r#type: "range",
+                 name: "chill_val",
+                 value: "{chill_val}",
+                 id: "chill_val",
+                 min: "10",
+                 max: "800",
+                 oninput: move |ev| chill_val.set(ev.value.clone()),
              }
-        ),
-    };
+         }
+         p { "chill: {chill_val}"}
+
+         Segments {fac: chill_val.clone(), now: **now}
+
+
+     }
+    );
     cx.render(content)
 }
--- util/web_server.py
+++ util/web_server.py
@@ -1,3 +1,4 @@
+import cgi
 from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 from sys import argv
 import time
@@ -7,6 +8,9 @@ hostName = "localhost"
 serverPort = 8081
 
 start = time.time()
+
+data = """{"2e2765f4-7fb7-4a8a-b61f-6dc874db15e9":{"uuid":"2e2765f4-7fb7-4a8a-b61f-6dc874db15e9","length":1,"bgr":false,"colors":[{"red":255,"green":150,"blue":0},{"red":255,"green":10,"blue":120}],"chill_idx":0,"chill_fac":100},"ae98126f-915e-470d-93a0-4b40a853a0c8":{"uuid":"ae98126f-915e-470d-93a0-4b40a853a0c8","length":1,"bgr":false,"colors":[{"red":166,"green":0,"blue":255},{"red":2,"green":192,"blue":192}],"chill_idx":1,"chill_fac":100},"bdc6cf10-c223-4c9e-9b94-88495d81617a":{"uuid":"bdc6cf10-c223-4c9e-9b94-88495d81617a","length":1,"bgr":false,"colors":[{"red":20,"green":200,"blue":141},{"red":200,"green":176,"blue":20}],"chill_idx":2,"chill_fac":100},"cba45b51-fd9a-48f4-95b3-070099050887":{"uuid":"cba45b51-fd9a-48f4-95b3-070099050887","length":1,"bgr":false,"colors":[{"red":200,"green":20,"blue":30},{"red":200,"green":200,"blue":10}],"chill_idx":3,"chill_fac":100}}""".encode(
+    'utf-8')
 
 
 class MyServer(SimpleHTTPRequestHandler):
@@ -24,23 +28,42 @@ class MyServer(SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'content-type')
         self.end_headers()
 
+    def do_POST(self):
+        self.send_response(204)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods',
+                         'POST')
+        self.send_header('Access-Control-Allow-Headers', 'content-type')
+
+        self.log_message("headers")
+
+        self.end_headers()
+        self.flush_headers()
+        global data
+        data = self.rfile.read()
+        self.log_message("read")
+        self.log_message("response")
+        self.wfile.write(b"ok")
+        # self.data = data.decode("utf-8")
+        print(data)
+
     def do_GET(self):
-        if self.path == '/now':
-            dt = int((time.time() - start)*1000)
+        def get(wat):
             self.send_response(200)
 
             self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Access-Control-Allow-Methods', 'GET')
-            self.send_header('Access-Control-Allow-Headers', 'content-type')
+            self.send_header('Access-Control-Allow-Methods', '*')
+            self.send_header('Access-Control-Allow-Headers', '*')
 
             self.end_headers()
-            self.wfile.write(f'{dt}'.encode('ascii'))
+            self.wfile.write(wat)
+
+        if self.path == '/now':
+            dt = int((time.time() - start)*1000)
+            get(f'{dt}'.encode('ascii'))
 
         elif self.path == '/data':
-            data = """{"2e2765f4-7fb7-4a8a-b61f-6dc874db15e9":{"uuid":"2e2765f4-7fb7-4a8a-b61f-6dc874db15e9","length":1,"bgr":false,"colors":[{"red":255,"green":150,"blue":0},{"red":255,"green":10,"blue":120}],"chill_idx":0,"chill_fac":100},"ae98126f-915e-470d-93a0-4b40a853a0c8":{"uuid":"ae98126f-915e-470d-93a0-4b40a853a0c8","length":1,"bgr":false,"colors":[{"red":166,"green":0,"blue":255},{"red":2,"green":192,"blue":192}],"chill_idx":1,"chill_fac":100},"bdc6cf10-c223-4c9e-9b94-88495d81617a":{"uuid":"bdc6cf10-c223-4c9e-9b94-88495d81617a","length":1,"bgr":false,"colors":[{"red":20,"green":200,"blue":141},{"red":200,"green":176,"blue":20}],"chill_idx":2,"chill_fac":100},"cba45b51-fd9a-48f4-95b3-070099050887":{"uuid":"cba45b51-fd9a-48f4-95b3-070099050887","length":1,"bgr":false,"colors":[{"red":200,"green":20,"blue":30},{"red":200,"green":200,"blue":10}],"chill_idx":3,"chill_fac":100}}"""
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(data.encode('utf-8'))
+            get(data)
         else:
             super().do_GET()
 
